5b604ba097a833a56dd6a7ba04d865d95bda7b51
garg1337 <ArjunPGarg@gmail.com>
2021-08-16 18:53:35 -0500

    pic grid

---
+++ Chap4/picgrid.py
@@ -0,0 +1,18 @@
+grid = [
+['.', '.', '.', '.', '.', '.'],
+['.', 'O', 'O', '.', '.', '.'],
+['O', 'O', 'O', 'O', '.', '.'],
+['O', 'O', 'O', 'O', 'O', '.'],
+['.', 'O', 'O', 'O', 'O', 'O'],
+['O', 'O', 'O', 'O', 'O', '.'],
+['O', 'O', 'O', 'O', '.', '.'],
+['.', 'O', 'O', '.', '.', '.'],
+['.', '.', '.', '.', '.', '.']]
+
+numRows = len(grid)
+numCols = len(grid[0])
+for i in range(numCols):
+    colstr = ''
+    for j in range(numRows):
+        colstr += grid[j][i]
+    print(colstr)
